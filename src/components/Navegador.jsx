8179501530b52f726518d79f234ee2b2485e752d
Lourdes Billone <lou.billone@gmail.com>
2024-10-10 15:26:04 -0300

    creo links en navegador

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const Navegador = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            React-Bootstrap
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link as={NavLink} to="/">
+                INICIO
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/vehiculos">
+                VEHÍCULOS
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/quienesSomos">
+                QUIENES SOMOS
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/contacto">
+                CONTACTO
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 };
 
 export default Navegador;
