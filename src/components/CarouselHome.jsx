bda9d5be511afee08410d1877b86db3b23f6a265
Lourdes Billone <lou.billone@gmail.com>
2024-11-09 16:44:17 -0300

    agrego textos y saco controles para que funcione solo

--- src/components/CarouselHome.jsx
+++ src/components/CarouselHome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Carousel from "react-bootstrap/Carousel";
-import renaulBigster from "../assets/image/carousel/renaultBigster.png";
+import renaultBigster from "../assets/image/carousel/renaultBigster.png";
 import vvGolf from "../assets/image/carousel/vwGolf.png";
 import toyotaHilux from "../assets/image/carousel/toyotaHilux.png";
 import fordRanger from "../assets/image/carousel/fordRanger.png";
@@ -8,39 +8,58 @@ import fordRanger from "../assets/image/carousel/fordRanger.png";
 const CarouselHome = () => {
   return (
     <div>
-      <Carousel>
+      <Carousel
+        controls={false}
+        indicators={false}
+        interval={3000}
+        pause={false}
+      >
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src={renaulBigster}
-            alt="First slide"
+            src={renaultBigster}
+            alt="renault bigster"
           />
           <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={vvGolf} alt="Second slide" />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={toyotaHilux} alt="Third slide" />
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
+            <h3>RENAULT BIGSTER</h3>
             <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              ENCUENTRA EL AUTO PERFECTO: COMPRA Y VENTA DE OK Y USADOS CON
+              SEGURIDAD Y GARANTÍA
             </p>
+            <button className="btn btn-primary">CONTACTAR</button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
-          <img className="d-block w-100" src={fordRanger} alt="Fourth slide" />
+          <img className="d-block w-100" src={vvGolf} alt="volkswagen golf" />
           <Carousel.Caption>
-            <h3>Fourth slide label</h3>
-            <p>Some descriptive text for the fourth slide.</p>
+            <h3>VOLKSWAGEN GOLF</h3>
+            <p>
+              ENCUENTRA EL AUTO PERFECTO: COMPRA Y VENTA DE OK Y USADOS CON
+              SEGURIDAD Y GARANTÍA
+            </p>
+            <button className="btn btn-primary">CONTACTAR</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={toyotaHilux} alt="toyota hilux" />
+          <Carousel.Caption>
+            <h3>TOYOTA HILUX</h3>
+            <p>
+              ENCUENTRA EL AUTO PERFECTO: COMPRA Y VENTA DE OK Y USADOS CON
+              SEGURIDAD Y GARANTÍA
+            </p>
+            <button className="btn btn-primary">CONTACTAR</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img className="d-block w-100" src={fordRanger} alt="ford ranger" />
+          <Carousel.Caption>
+            <h3>FORD RANGER</h3>
+            <p>
+              ENCUENTRA EL AUTO PERFECTO: COMPRA Y VENTA DE OK Y USADOS CON
+              SEGURIDAD Y GARANTÍA
+            </p>
+            <button className="btn btn-primary">CONTACTAR</button>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
