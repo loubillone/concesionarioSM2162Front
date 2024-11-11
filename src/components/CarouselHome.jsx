2734cf62e6901fc733b5ad63e4679835032db827
Lourdes Billone <lou.billone@gmail.com>
2024-11-11 11:58:35 -0300

    texto de carousel con tipografia

--- src/components/CarouselHome.jsx
+++ src/components/CarouselHome.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../css/carouselHome.css";
 import Carousel from "react-bootstrap/Carousel";
 import renaultBigster from "../assets/image/carousel/renaultBigster.png";
 import vvGolf from "../assets/image/carousel/vwGolf.png";
@@ -9,57 +10,72 @@ const CarouselHome = () => {
   return (
     <div>
       <Carousel
+        // controls={false}
+        // indicators={false}
+        // interval={3000}
+        // // pause={true}
         controls={false}
         indicators={false}
-        interval={3000}
-        pause={false}
+        interval={null}
+        pause={"hover"}
       >
-        <Carousel.Item>
+        <Carousel.Item className="carousel-item">
           <img
             className="d-block w-100"
             src={renaultBigster}
             alt="renault bigster"
           />
-          <Carousel.Caption>
-            <h3>RENAULT BIGSTER</h3>
-            <p>
+          <Carousel.Caption className="contenedor-texto-carousel">
+            <h3 className="titulo-item-carousel">RENAULT BIGSTER</h3>
+            <p className="texto-item-carousel">
               ENCUENTRA EL AUTO PERFECTO: COMPRA Y VENTA DE OK Y USADOS CON
               SEGURIDAD Y GARANTÍA
             </p>
-            <button className="btn btn-primary">CONTACTAR</button>
+            <button className="btn btn-primary boton-item-carousel">
+              CONTACTAR
+            </button>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+        <Carousel.Item className="carousel-item">
           <img className="d-block w-100" src={vvGolf} alt="volkswagen golf" />
-          <Carousel.Caption>
-            <h3>VOLKSWAGEN GOLF</h3>
-            <p>
+          <Carousel.Caption className="contenedor-texto-carousel">
+            <h3 className="titulo-item-carousel">VOLKSWAGEN GOLF</h3>
+            <p className="texto-item-carousel">
               ENCUENTRA EL AUTO PERFECTO: COMPRA Y VENTA DE OK Y USADOS CON
               SEGURIDAD Y GARANTÍA
             </p>
-            <button className="btn btn-primary">CONTACTAR</button>
+            <button className="btn btn-primary boton-item-carousel">
+              CONTACTAR
+            </button>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+        <Carousel.Item className="carousel-item">
           <img className="d-block w-100" src={toyotaHilux} alt="toyota hilux" />
-          <Carousel.Caption>
-            <h3>TOYOTA HILUX</h3>
-            <p>
+          <Carousel.Caption className="contenedor-texto-carousel">
+            <h3 className="titulo-item-carousel">TOYOTA HILUX</h3>
+            <p className="texto-item-carousel">
               ENCUENTRA EL AUTO PERFECTO: COMPRA Y VENTA DE OK Y USADOS CON
               SEGURIDAD Y GARANTÍA
             </p>
-            <button className="btn btn-primary">CONTACTAR</button>
+            <button className="btn btn-primary boton-item-carousel">
+              CONTACTAR
+            </button>
           </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Item>
+
+        <Carousel.Item className="carousel-item">
           <img className="d-block w-100" src={fordRanger} alt="ford ranger" />
-          <Carousel.Caption>
-            <h3>FORD RANGER</h3>
-            <p>
+          <Carousel.Caption className="contenedor-texto-carousel">
+            <h3 className="titulo-item-carousel">FORD RANGER</h3>
+            <p className="texto-item-carousel">
               ENCUENTRA EL AUTO PERFECTO: COMPRA Y VENTA DE OK Y USADOS CON
               SEGURIDAD Y GARANTÍA
             </p>
-            <button className="btn btn-primary">CONTACTAR</button>
+            <button className="btn btn-primary boton-item-carousel">
+              CONTACTAR
+            </button>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
